fa66fa99906b56cb8256d1b15d6e30e0bc9d16cb
Muhammad7878 <58218179+Muhammad7878@users.noreply.github.com>
2021-04-05 14:35:46 +0200

    Logic created.

--- src/game.js
+++ src/game.js
@@ -1,6 +1,36 @@
 "use strict"
+let location1=3;
+let location2=4;
+let location3=5;
+
+let guess;
+let hits=0;
+let guesses=0;
+
+let isSunk = false;
 
 document.addEventListener("DOMContentLoaded", ()=>{
+  while(isSunk == false){
+    guess = prompt("FEUER!!! (Gib eine Zahl zwischen 0-6): ")
 
-  alert("Hello")
+    if(guess < 0 || guess > 6){
+      alert("Bitte gib eine gültige Zahl an!")
+    }else{
+      guesses ++;
+    }
+
+    if(guess == location1 || guess == location2 || guess == location3){
+      hits = hits + 1;
+      alert("TREFFER");
+
+            if(hits == 3){
+              isSunk = true;
+              alert ("Der Schiff wurde vernichtet!");
+            }
+  } else{
+    alert("LOL... Versuchs noch Mal!!!")
+  }
+}
+  let stats = "Du hast "+ guesses +" Versuche gebraucht, um das Schiff zu vernichten. Die Genauikgeit beträgt " + (3/guesses);
+  alert(stats);
 })
