1d2799a11f167c44cc50212c351f2603f98c16f5
Muhammad7878 <58218179+Muhammad7878@users.noreply.github.com>
2021-04-05 21:59:14 +0200

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,11 +1,16 @@
 "use strict"
-let location1=3;
-let location2=4;
-let location3=5;
+
+// Multiplying Math.random with 5 to get a random Number between 0 and 4.99.
+//Math.floor to round it.
+let randomLoc = Math.floor(Math.random()*5);
+let location1=randomLoc;
+let location2=location1 + 1;
+let location3=location2 + 1;
 
 let guess;
 let hits=0;
 let guesses=0;
+let hitLocations = [];
 
 let isSunk = false;
 
@@ -17,19 +22,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
       alert("Bitte gib eine gültige Zahl an!")
     }else{
       guesses ++;
+      hitLocations.push(guess);
     }
 
     if(guess == location1 || guess == location2 || guess == location3){
-      hits = hits + 1;
-      alert("TREFFER");
+       hits = hits + 1;
+       alert("TREFFER");
 
-            if(hits == 3){
-              isSunk = true;
-              alert ("Der Schiff wurde vernichtet!");
-            }
-  } else{
-    alert("LOL... Versuchs noch Mal!!!")
-  }
+         if(hits == 3){
+             isSunk = true;
+             alert ("Der Schiff wurde vernichtet!");
+           }
+    } else{
+       alert("LOL... Versuchs noch Mal!!!")
+    }
+
 }
   let stats = "Du hast "+ guesses +" Versuche gebraucht, um das Schiff zu vernichten. Die Genauikgeit beträgt " + (3/guesses);
   alert(stats);
